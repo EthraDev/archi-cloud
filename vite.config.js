655f21874b561e1a4abffdbf1e16e0359ea8b13a
theab <thea.blachon@veolia.com>
2025-07-21 11:30:12 +0200

    add testu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// 👇 ajoute ce bloc "test" pour activer les globals
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
